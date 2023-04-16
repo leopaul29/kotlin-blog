1bd4781f4a2f2c098f6c5f6e504ae8b414eda97b
leo <leopaulmartin@hotmail.fr>
2023-04-16 19:27:10 +0200

    temp

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -1,5 +1,6 @@
 package com.example.blog
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
